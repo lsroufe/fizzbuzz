1ce0313ff78e48f6891f226d4627c2b4206b555b
Logan Sroufe <lsroufe@gmail.com>
2016-06-02 09:11:01 -0700

    updated javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-function fizzBuzz() {
 for ( var count = 1; count <= 100; count++) {
 		if (count % 3 === 0 && count % 5 === 0) {
 			$('body').append("<li>FizzBuzz</li>");
@@ -13,6 +12,3 @@ for ( var count = 1; count <= 100; count++) {
 			$('body').append("<li>" + count + "</li>");
 		}
 	}
-  }
-fizzBuzz();
-
