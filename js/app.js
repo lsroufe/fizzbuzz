c423baa1d84f864ecb182e9d275586b2dba2aaed
Logan Sroufe <lsroufe@gmail.com>
2016-06-02 08:47:36 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function fizzBuzz(){
+for ( var count = 1; count <= 100; count++) {
+		if (count % 3 === 0 && count % 5 === 0) {
+			$('body').append("<li>FizzBuzz!</li>");
+		}
+		else if (count % 3 === 0) {
+			$('body').append("<li>Fizz</li>");
+		}
+		else if (count % 5 === 0) {
+			$('body').append("<li>Buzz</li>");
+		}
+		else {
+			$('body').append("<li>" + count + "</li>");
+		}
+	}
+  }
+fizzBuzz();
+
