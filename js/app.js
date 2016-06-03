ef0aa35e274106fa02a78dc8fa6aced5fa8c8457
Logan Sroufe <lsroufe@gmail.com>
2016-06-02 20:53:50 -0700

    refactored fizzbuzz js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,35 @@
-for ( var count = 1; count <= 100; count++) {
-		if (count % 3 === 0 && count % 5 === 0) {
+// for ( var count = 1; count <= 100; count++) {
+// 		if (count % 3 === 0 && count % 5 === 0) {
+// 			$('body').append("<li>FizzBuzz</li>");
+// 		}
+// 		else if (count % 3 === 0) {
+// 			$('body').append("<li>Fizz</li>");
+// 		}
+// 		else if (count % 5 === 0) {
+// 			$('body').append("<li>Buzz</li>");
+// 		}
+// 		else {
+// 			$('body').append("<li>" + count + "</li>");
+// 		}
+// 	}
+
+
+// FizzBuzz Refactor
+$(document).ready(function(e) {
+
+var usrInput = parseInt(prompt())
+
+		if (usrInput % 3 === 0 && usrInput % 5 === 0) {
 			$('body').append("<li>FizzBuzz</li>");
 		}
-		else if (count % 3 === 0) {
+		else if (usrInput % 3 === 0) {
 			$('body').append("<li>Fizz</li>");
 		}
-		else if (count % 5 === 0) {
+		else if (usrInput % 5 === 0) {
 			$('body').append("<li>Buzz</li>");
 		}
 		else {
-			$('body').append("<li>" + count + "</li>");
+			$('body').append("<li>" + usrInput + "</li>");
 		}
-	}
+
+});
